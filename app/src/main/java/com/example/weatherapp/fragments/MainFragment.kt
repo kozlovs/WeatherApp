a7d3691ab810b3a64f4fa11c19abf45f7931f28d
Sergey Kozlov <104564146+kozlovs@users.noreply.github.com>
2022-10-01 13:01:47 +1000

    Запрос погоды перенесен во ViewModel

--- app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,25 +17,17 @@ import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.example.weatherapp.DialogManager
 import com.example.weatherapp.adapters.VpAdapter
-import com.example.weatherapp.adapters.WeatherModel
 import com.example.weatherapp.databinding.FragmentMainBinding
 import com.example.weatherapp.isPermissionGranted
 import com.example.weatherapp.viewmodel.MainViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.material.tabs.TabLayoutMediator
 import com.squareup.picasso.Picasso
-import org.json.JSONObject
-
-const val API_KEY = "f6851a729736467aa33123131222409"
 
 class MainFragment : Fragment() {
     private lateinit var fLocation: FusedLocationProviderClient
@@ -76,12 +67,11 @@ class MainFragment : Fragment() {
             checkLocation()
         }
         ibSearch.setOnClickListener {
-            DialogManager.searchByNameDialog(requireContext(), object : DialogManager.Listener{
+            DialogManager.searchByNameDialog(requireContext(), object : DialogManager.Listener {
                 override fun onClick(name: String?) {
-                    name?.let { requestWeatherData(it) }
+                    name?.let { viewModel.requestWeatherData(context, it) }
                 }
             })
-
         }
     }
 
@@ -108,16 +98,19 @@ class MainFragment : Fragment() {
             return
         }
         fLocation.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, ct.token)
-            .addOnCompleteListener() {
-            requestWeatherData("${it.result.latitude},${it.result.longitude}")
-        }
+            .addOnCompleteListener {
+                viewModel.requestWeatherData(
+                    context,
+                    "${it.result.latitude},${it.result.longitude}"
+                )
+            }
     }
 
     private fun checkLocation() {
         if (isLocationEnabled()) {
             getLocation()
         } else {
-            DialogManager.locationSettingsDialog(requireContext(), object : DialogManager.Listener{
+            DialogManager.locationSettingsDialog(requireContext(), object : DialogManager.Listener {
                 override fun onClick(name: String?) {
                     startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
                 }
@@ -138,26 +131,6 @@ class MainFragment : Fragment() {
         }
     }
 
-    private fun requestWeatherData(cityName: String) {
-        val url = "https://api.weatherapi.com/v1/forecast.json?" +
-                "key=$API_KEY" +
-                "&q=$cityName" +
-                "&days=3" +
-                "&aqi=no" +
-                "&alerts=no"
-        val queue = Volley.newRequestQueue(context)
-        val stringRequest = StringRequest(
-            Request.Method.GET,
-            url,
-            { result -> parseWeatherData(result)
-            },
-            { error ->
-                Log.d("MyLog", "Error: $error")
-            }
-        )
-        queue.add(stringRequest)
-    }
-
     private fun updateCurrentCard() = with(binding) {
         viewModel.liveDataCurrent.observe(viewLifecycleOwner) {
             val minMaxTemp = "${it.minTemp}C / ${it.maxTemp}C"
@@ -166,55 +139,9 @@ class MainFragment : Fragment() {
             tvCity.text = it.city
             tvCurrentTemp.text = if (it.currentTemp.isEmpty()) minMaxTemp else currentTemp
             tvCondition.text = it.condition
-            tvMaxMin.text = if(it.currentTemp.isEmpty()) "" else minMaxTemp
+            tvMaxMin.text = if (it.currentTemp.isEmpty()) "" else minMaxTemp
             Picasso.get().load("https:${it.imageUrl}").into(imWeather)
         }
-    }
-
-    private fun parseWeatherData(result: String) {
-        val mainObject = JSONObject(result)
-        val list = parseDays(mainObject)
-        parseCurrentData(mainObject, list[0])
-    }
-
-    private fun parseCurrentData(mainObject: JSONObject, weatherItem: WeatherModel) {
-        val item = WeatherModel(
-            mainObject.getJSONObject("location").getString("name"),
-            mainObject.getJSONObject("current").getString("last_updated"),
-            mainObject.getJSONObject("current")
-                .getJSONObject("condition").getString("text"),
-            mainObject.getJSONObject("current")
-                .getJSONObject("condition").getString("icon"),
-            mainObject.getJSONObject("current").getString("temp_c"),
-            weatherItem.maxTemp,
-            weatherItem.minTemp,
-            weatherItem.hours
-        )
-        viewModel.liveDataCurrent.value = item
-    }
-
-    private fun parseDays(mainObject: JSONObject) : List<WeatherModel> {
-        val list = ArrayList<WeatherModel>()
-        val daysArray = mainObject
-            .getJSONObject("forecast")
-            .getJSONArray("forecastday")
-        val name = mainObject.getJSONObject("location").getString("name")
-        for (i in 0 until daysArray.length()) {
-            val day = daysArray[i] as JSONObject
-            val item = WeatherModel(
-                name,
-                day.getString("date"),
-                day.getJSONObject("day").getJSONObject("condition").getString("text"),
-                day.getJSONObject("day").getJSONObject("condition").getString("icon"),
-                "",
-                day.getJSONObject("day").getString("maxtemp_c"),
-                day.getJSONObject("day").getString("mintemp_c"),
-                day.getJSONArray("hour").toString()
-            )
-            list += item
-        }
-        viewModel.liveDataList.value = list
-        return list
     }
 
     companion object {
--- app/src/main/java/com/example/weatherapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/viewmodel/MainViewModel.kt
@@ -1,10 +1,83 @@
 package com.example.weatherapp.viewmodel
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.weatherapp.R
 import com.example.weatherapp.adapters.WeatherModel
+import org.json.JSONObject
 
 class MainViewModel : ViewModel() {
     val liveDataCurrent = MutableLiveData<WeatherModel>()
     val liveDataList = MutableLiveData<List<WeatherModel>>()
+
+    fun requestWeatherData(context: Context?, cityName: String) {
+        val url = "https://api.weatherapi.com/v1/forecast.json?" +
+                "key=${context?.getString(R.string.api_key)}" +
+                "&q=$cityName" +
+                "&days=3" +
+                "&aqi=no" +
+                "&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            url,
+            { result -> parseWeatherData(result)
+            },
+            { error ->
+                Log.d("MyLog", "Error: $error")
+            }
+        )
+        queue.add(stringRequest)
+    }
+
+    private fun parseWeatherData(result: String) {
+        val mainObject = JSONObject(result)
+        val list = parseDays(mainObject)
+        parseCurrentData(mainObject, list[0])
+    }
+
+    private fun parseCurrentData(mainObject: JSONObject, weatherItem: WeatherModel) {
+        val item = WeatherModel(
+            mainObject.getJSONObject("location").getString("name"),
+            mainObject.getJSONObject("current").getString("last_updated"),
+            mainObject.getJSONObject("current")
+                .getJSONObject("condition").getString("text"),
+            mainObject.getJSONObject("current")
+                .getJSONObject("condition").getString("icon"),
+            mainObject.getJSONObject("current").getString("temp_c"),
+            weatherItem.maxTemp,
+            weatherItem.minTemp,
+            weatherItem.hours
+        )
+        liveDataCurrent.value = item
+    }
+
+    private fun parseDays(mainObject: JSONObject) : List<WeatherModel> {
+        val list = ArrayList<WeatherModel>()
+        val daysArray = mainObject
+            .getJSONObject("forecast")
+            .getJSONArray("forecastday")
+        val name = mainObject.getJSONObject("location").getString("name")
+        for (i in 0 until daysArray.length()) {
+            val day = daysArray[i] as JSONObject
+            val item = WeatherModel(
+                name,
+                day.getString("date"),
+                day.getJSONObject("day").getJSONObject("condition").getString("text"),
+                day.getJSONObject("day").getJSONObject("condition").getString("icon"),
+                "",
+                day.getJSONObject("day").getString("maxtemp_c"),
+                day.getJSONObject("day").getString("mintemp_c"),
+                day.getJSONArray("hour").toString()
+            )
+            list += item
+        }
+        liveDataList.value = list
+        return list
+    }
 }
